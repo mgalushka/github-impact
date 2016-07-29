af0b753bf7b7b811a71747ca8d165f4ae4761890
Maxim Galushka <mgalushka@gmail.com>
2016-07-30 00:35:49 +0100

    more restrictions

--- impact_search.py
+++ impact_search.py
@@ -14,7 +14,7 @@ def start_script(auth_string):
         print("Requesting page={0}".format(page))
         time.sleep(2)
         request = urllib2.Request((
-            '{root}/search/repositories?q=stars:<300+language:cpp&sort=stars&'
+            '{root}/search/repositories?database&q=stars:<50+language:cpp&sort=stars&'
             'order=desc&page={page}&per_page=100'
         ).format(
             root=ROOT,
@@ -45,10 +45,13 @@ def start_script(auth_string):
 
             m = json.loads(maintainers.read())
             contributors_count = len(m)
-            open_issues = item['open_issues_count']
-            stars = item['stargazers_count']
-            forks = item['forks_count']
-            watchers = item['watchers']
+            open_issues = int(item['open_issues_count'])
+            stars = int(item['stargazers_count'])
+            forks = int(item['forks_count'])
+            watchers = int(item['watchers'])
+
+            if contributors_count < 5 or open_issues < 5:
+                continue
 
             # time.sleep(6)
             request = urllib2.Request(
@@ -59,18 +62,21 @@ def start_script(auth_string):
             request.add_header("Authorization", "Basic {0}".format(auth_string))
             commits = urllib2.urlopen(request)
 
-            c = json.loads(commits.read())
+            # let's see how many commits happened to project recently
+            # this is to evaluate how active this is
+            project_commits = json.loads(commits.read())
             active_project = False
             last_commit = 'never'
 
-            if len(c) > 0:
-                last_commit = c[0]['commit']['committer']['date'][:10]
+            if len(project_commits) > 0:
+                last_commit = \
+                    project_commits[0]['commit']['committer']['date'][:10]
                 count = 0
                 date_now = datetime.now()
-                for commit in c:
+                LAST_30 = timedelta(days=-30)
+                for commit in project_commits:
                     dt = commit['commit']['committer']['date'][:10]
-                    if datetime.strptime(dt, '%Y-%m-%d') > date_now + timedelta(
-                            days=-30):
+                    if datetime.strptime(dt, '%Y-%m-%d') > date_now + LAST_30:
                         active_project = True
                     else:
                         active_project = False
